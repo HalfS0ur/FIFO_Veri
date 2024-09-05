1ad52e78854cc1f8903bcd3d003f832909cd4653
HalfS0ur <riveras.kendall99@gmail.com>
2024-09-05 00:43:00 -0600

    Modified checker.sv

--- checker.sv
+++ checker.sv
@@ -1,13 +1,15 @@
+//Checker y scoreboard: Verifica que el comportamiento del DUT sea el esperado
+//---------------------------------------------------------------------------//
 class checker #(parameter width=16, parameter depth =8);
-trans_fifo #(.width(width)) transaccion; 
-trans_fifo #(.width(width)) auxiliar; 
-trans_sb   #(.width(width)) to_sb; 
-trans_fifo  emul_fifo[$]; 
-trans_fifo_mbx drv_chkr_mbx; 
-trans_sb_mbx  chkr_sb_mbx; 
+trans_fifo #(.width(width)) transaccion; //transaccion recibida en el mb
+trans_fifo #(.width(width)) auxiliar; //tansaccion auxiliar para simular el fifo
+trans_sb   #(.width(width)) to_sb; //transaccion para comunicarse con el scoreboard
+trans_fifo  emul_fifo[$]; //queue de golden reference para el fifo
+trans_fifo_mbx drv_chkr_mbx; //Mailbox para comunicar el driver/monitor
+trans_sb_mbx  chkr_sb_mbx; //Mailbox para comunicar checker y scoreboard
 int contador_auxiliar; 
 
-function new();
+function new(); //Constructor del checker
   this.emul_fifo = {};
   this.contador_auxiliar = 0;
 endfunction 
@@ -27,7 +29,7 @@ task run;
          if(transaccion.dato == auxiliar.dato) begin
            to_sb.dato_enviado = auxiliar.dato;
            to_sb.tiempo_push = auxiliar.tiempo;
-           to_sb.tiempo_pop = transaccion.dato;
+           to_sb.tiempo_pop = transaccion.dato; //What?
            to_sb.completado = 1;
            to_sb.calc_latencia();
            to_sb.print("Checker:Transaccion Completada");
@@ -37,7 +39,7 @@ task run;
           $display("Dato_leido= %h, Dato_Esperado = %h",transaccion.dato,auxiliar.dato);
           $finish; 
          end
-       end else begin // si está vacía genera un underflow 
+       end else begin //Generar underflow si el fifo esta vacio
            to_sb.tiempo_pop = transaccion.tiempo;
            to_sb.underflow = 1;
            to_sb.print("Checker: Underflow");
@@ -45,7 +47,7 @@ task run;
        end
      end
      escritura: begin
-       if(emul_fifo.size() == depth)begin // Revisa si la Fifo está llena para generar un overflow
+       if(emul_fifo.size() == depth)begin //Genera overflow si la fifo esta llena
          auxiliar = emul_fifo.pop_front();
          to_sb.dato_enviado = auxiliar.dato;
          to_sb.tiempo_push = auxiliar.tiempo;
@@ -53,12 +55,12 @@ task run;
          to_sb.print("Checker: Overflow");
          chkr_sb_mbx.put(to_sb);
          emul_fifo.push_back(transaccion);
-       end else begin 
+       end else begin //Si no esta llena meter el dato en la fifo simulada
          transaccion.print("Checker: Escritura");
          emul_fifo.push_back(transaccion);
        end
      end
-     reset: begin
+     reset: begin //Vaciar la fifo simulada y mandar los datos perdidos al scoreboard
        contador_auxiliar = emul_fifo.size();
        for(int i =0; i<contador_auxiliar; i++)begin
          auxiliar = emul_fifo.pop_front();
@@ -71,13 +73,13 @@ task run;
        end
      end
      lectoescritura: begin
-      if (emul_fifo.size == 0) begin
+      if (emul_fifo.size == 0) begin //Si el fifo esta vacio generar un underflow
         to_sb.tiempo_pop = transaccion.tiempo;
         to_sb.underflow = 1;
         to_sb.print("Checker: Underflow");
         chkr_sb_mbx.put(to_sb);
       end
-      else if (emul_fifo.size() == depth) begin
+      else if (emul_fifo.size() == depth) begin //Si el fifo esta lleno generar un overflow
         auxiliar = emul_fifo.pop_front();
         to_sb.dato_enviado = auxiliar.dato;
         to_sb.tiempo_push = auxiliar.tiempo;
@@ -86,7 +88,7 @@ task run;
         chkr_sb_mbx.put(to_sb);
         emul_fifo.push_back(transaccion);
       end
-      else begin
+      else begin //Si no hay overflow ni underflow
         transaccion.print("Checker: Lectoescritura");
         emul_fifo.push_back(transaccion);
         auxiliar = emul_fifo.pop_front();
